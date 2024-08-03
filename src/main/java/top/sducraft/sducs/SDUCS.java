2ffa3fc774c2d2fad5467f7616c5f10f444b8792
Cyrilic <60803006+Cyrilic1209@users.noreply.github.com>
2024-08-03 11:07:55 +0800

    Initial commit

---
+++ src/main/java/top/sducraft/sducs/SDUCS.java
@@ -0,0 +1,17 @@
+package top.sducraft.sducs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SDUCS extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
